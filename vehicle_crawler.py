76ea553560bbf94bb2d209de0def3a2371f1ab26
enter1230 <enter123123@naver.com>
2021-05-18 23:21:49 +0900

    final crawler is created!

--- vehicle_crawler.py
+++ vehicle_crawler.py
@@ -27,11 +27,8 @@ for item in allBikeCompanyElement:
 
 nextBtn = driver.find_element_by_css_selector("#_vendor_select_layer > div > div.maker_group > div.rolling_btn > button.next")
 
+
 for i in range(1):
-    isExistNextPage = nextBtn.is_enabled()
-
-
-if (isExistNextPage == True):
     print("다음 페이지 존재함=======================================>")
     nextBtn.click()
     allBikeCompanyElement = driver.find_elements_by_css_selector("#_vendor_select_layer > div > div.maker_group div.emblem_area > ul > li")
@@ -43,3 +40,33 @@ if (isExistNextPage == True):
             print('네이버 자동차 바이크제조사 홈 sub url:', ahref)
             imgUrl = item.find_element_by_tag_name("img").get_attribute("src")
             print('바이크 회사 엠블럼:', imgUrl)
+
+def crawlingpage():
+    allCarCompanyElement = driver.find_elements_by_css_selector("#_vendor_select_layer > div > div.maker_group > div.emblem_area > ul > li")
+    for item in allCarCompanyElement:
+        carComName = item.find_element_by_tag_name("span").text
+        if (carComName != ''):
+            print("자동차 회사명:" + carComName)
+            ahref = item.find_element_by_tag_name("a").get_attribute("href")
+            print('네이버 자동차 자동차제조사 홈 sub url:', ahref)
+            imgUrl = item.find_element_by_tag_name("img").get_attribute("src")
+            print('자동차 회사 엠블럼:', imgUrl)
+
+chromedriver = 'D:/Users/Master/PycharmProjects/chromedriver.exe'
+driver = webdriver.Chrome(chromedriver)
+
+driver.get('https://auto.naver.com/car/mainList.nhn')
+
+print(driver.title)
+print(driver.current_url)
+
+carCompanyAll = driver.find_element_by_css_selector("#container > div.spot_main > div.spot_aside > div.tit > a")
+carCompanyAll.click()
+
+next = driver.find_element_by_css_selector("#_vendor_select_layer > div > div.maker_group > div.rolling_btn > button.next")
+
+crawlingpage()
+for i in range(3):
+    print("다음 페이지")
+    next.click()
+    crawlingpage()
